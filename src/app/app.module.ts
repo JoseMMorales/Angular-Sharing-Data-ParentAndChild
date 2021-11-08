dc81231b9c7182791ad3a88394b3fdeab04bfe1e
JoseMMorales <josemulamorales33@hotmail.com>
2021-11-08 20:22:07 +0100

    Adding styles to parent and child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
 import { BeveragesComponent } from './beverages/beverages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent,
     BeveragesComponent
   ],
   imports: [
